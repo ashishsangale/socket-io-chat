eed88b5a8d9175d24dfed366192e70dbc5ac4e2e
Ashish Sangale <aashishsangale07@gmail.com>
2020-05-06 22:07:47 +0530

    logging user messages

--- app.js
+++ app.js
@@ -9,4 +9,26 @@ const io = socketio(server);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-server.listen(process.env.PORT || 3000, ()=> console.log("starting......."));
+io.on('connection', socket => {
+    console.log('new connection...')
+
+
+    socket.emit('message', 'Welcome to PiperChat');
+
+
+    //Broadcast when user connects
+    socket.broadcast.emit('message', 'A user has connected');
+    
+    //when user disconnects
+    socket.on('disconnect', () => {
+        io.emit('message', "User left chat");
+    });
+
+    //listen to chat message
+    socket.on('chatMessage', (msg) => {
+        io.emit('message', msg);
+    });
+
+});
+
+server.listen(process.env.PORT || 3000, () => console.log("starting......."));
